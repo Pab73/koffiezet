9c42413ed8f5334e2f7aa3286617907507d968fc
PauloZM <Munoz.pablito@Gmail.com>
2021-04-01 08:43:38 +0200

    Initial commit

---
+++ WpfApp31.4/Koffiezet.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace WpfApp31._4
+{
+    public class Koffiezet
+    {
+        private int _pakBonen;
+        private int _bonenReservoir = 15;
+        private bool _power;
+        private bool _isPouring;
+        private string _statusKoffieSchenken;
+
+        public delegate void KoffieHandler();
+        public event KoffieHandler KoffiezetAanEvent;
+        public event KoffieHandler KoffiezetUitEvent;
+        public event KoffieHandler KoffieSchenkEvent;
+        public event KoffieHandler koffieStopSchenkEvent;
+
+        private DispatcherTimer _tim = new DispatcherTimer();
+        private DateTime _startuur;
+
+
+        public DispatcherTimer Tim
+        {
+            get => _tim;
+            //set { _tim = value; }
+        }
+        private DateTime startuur
+        {
+            get => _startuur;
+            set
+            {
+                _startuur = value;
+            }
+        }
+
+        public Koffiezet()
+        {
+            Tim.Interval = new TimeSpan(0, 0, 1);
+            Tim.Tick += Tim_Tick;
+        }
+        public void ZetDeKoffie()
+        {
+            if (Power)
+            {
+                StatusKoffieSchenken = "YOUR COFFEE IS BEING MADE...";
+                IsPouring = true;
+                startuur = DateTime.Now;
+                Tim.Stop();
+                Tim.Start();
+                BonenReservoir -= 5;
+
+            }
+        }
+
+        void Tim_Tick(object sender, EventArgs e)
+        {
+            using (SoundPlayer mPlayer = new SoundPlayer("C://Users/XYZ/source/repos/WpfApp31.4/WpfApp31.4/Sound/pouring-water-to-coffee-maker.wav"))
+            {
+                mPlayer.PlaySync();
+            }
+            DateTime eindtijd = startuur.AddSeconds(7.5);
+            if (eindtijd <= DateTime.Now)
+            {
+                Tim.Stop();
+                IsPouring = false;
+            }
+        }
+
+        public string StatusKoffieSchenken
+        {
+            get => _statusKoffieSchenken;
+            set => _statusKoffieSchenken = value;
+        }
+
+        public int PakBonen
+        {
+            get => _pakBonen;
+            set
+            {
+                _pakBonen = 30;
+            }
+        }
+        public int BonenReservoir
+        {
+            get => _bonenReservoir;
+            set
+            {
+                _bonenReservoir = value;
+            }
+        }
+
+        public bool Power
+        {
+            get => _power;
+            set
+            {
+                _power = value;
+                if (value == true)
+                {
+                    KoffiezetAanEvent?.Invoke();
+                }
+                else
+                {
+                    KoffiezetUitEvent?.Invoke();
+                }
+            }
+        }
+
+        public bool IsPouring
+        {
+            get => _isPouring;
+            set
+            {
+                _isPouring = value;
+                if (value == true)
+                {
+                    KoffieSchenkEvent?.Invoke();
+                }
+                else
+                {
+                    koffieStopSchenkEvent?.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp31.4/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace WpfApp31._4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        Koffiezet koffiezet = new Koffiezet();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            koffiezet.KoffiezetAanEvent += KoffiesetStaatAan;
+            koffiezet.KoffiezetUitEvent += KoffiesetStaatUit;
+            koffiezet.KoffieSchenkEvent += koffieSchenk;
+            koffiezet.koffieStopSchenkEvent += koffieStopSchenk;
+            koffiezet.Power = false;
+
+        }
+
+        private void koffieStopSchenk()
+        {
+            lblSchnekStatus.Content = "";
+            MessageBox.Show("YOUR COFFEE IS READY !", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void koffieSchenk()
+        {
+            lblSchnekStatus.Content = koffiezet.StatusKoffieSchenken;
+        }
+
+        private void KoffiesetStaatUit()
+        {
+            Background = Brushes.Black;
+            //geen koffie meer te krijgen
+            lblBonenReservoir.Content = $"STATUS: SLEEP, BEANS HOPPER: {koffiezet.BonenReservoir}  ";
+            lblSchnekStatus.Content = "";
+        }
+
+        private void KoffiesetStaatAan()
+        {
+            //mogelijkheid om koffie te zetten.
+            Background = Brushes.DarkCyan;
+            lblBonenReservoir.Content = $"BEANS HOPPER: {koffiezet.BonenReservoir} ";
+        }
+
+        private void btnKoffiezetAan_Click(object sender, RoutedEventArgs e)
+        {
+            koffiezet.Power = true;
+        }
+
+        private void btnKoffiezetUit_Click(object sender, RoutedEventArgs e)
+        {
+            koffiezet.Power = false;
+        }
+
+        private void btnEenKoffie_Click(object sender, RoutedEventArgs e)
+        {
+            if (koffiezet.Tim.IsEnabled)
+                return;
+            if (koffiezet.BonenReservoir <= 0 && koffiezet.Power)
+            {
+                BonenOpMenu();
+                return;
+            }
+            koffiezet.ZetDeKoffie();
+            if (koffiezet.Power)
+            {
+                lblBonenReservoir.Content = $"BEANS HOPPER: {koffiezet.BonenReservoir} ";
+            }
+        }
+
+        private void BonenOpMenu()
+        {
+            DialogResult dialogbox = MessageBox.Show("NO MORE BEANS, FEED THE HOPPER?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (dialogbox == System.Windows.Forms.DialogResult.Yes)
+            {
+                using (SoundPlayer mPlayer = new SoundPlayer("C://Users/XYZ/source/repos/WpfApp31.4/WpfApp31.4/Sound/seeds-cristal-jar-4.wav"))
+                {
+                    mPlayer.Play();
+                }
+                koffiezet.BonenReservoir = 15;
+                lblBonenReservoir.Content = $"BEANS HOPPER: {koffiezet.BonenReservoir} ";
+            }
+            else if (dialogbox == System.Windows.Forms.DialogResult.No)
+            {
+                koffiezet.Power = false;
+            }
+        }
+
+        private void btnVulBonen_Click(object sender, RoutedEventArgs e)
+        {
+            if (koffiezet.Tim.IsEnabled)
+                return;
+            if (koffiezet.Power && koffiezet.BonenReservoir < 15)
+            {
+                koffiezet.BonenReservoir = 15;
+                lblBonenReservoir.Content = $"BEANS HOPPER: {koffiezet.BonenReservoir} ";
+                using (SoundPlayer mPlayer = new SoundPlayer("C://Users/XYZ/source/repos/WpfApp31.4/WpfApp31.4/Sound/seeds-cristal-jar-4.wav"))
+                {
+                    mPlayer.Play();
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
